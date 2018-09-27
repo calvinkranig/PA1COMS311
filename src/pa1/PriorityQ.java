2efddadb9b1fc116ce9f460579f581de11df5b29
Alex <ans66@iastate.edu>
2018-09-27 17:11:55 -0500

    Added all of the required methods for the PriorityQ and javadoc comments

--- src/pa1/PriorityQ.java
+++ src/pa1/PriorityQ.java
@@ -1,5 +1,67 @@
 package pa1;
 
+/**
+ * 
+ * @author Alexander Stephens and Calvin Kranig
+ *Class for representing a Priority Queue.
+ */
 public class PriorityQ {
-
+	
+	/**
+	 * Creates an empty priority queue.
+	 */
+	public PriorityQ() {
+		
+	}
+	
+	/**
+	 * Adds a String s with priority p to the priority queue.
+	 * @param s String to be added to the queue.
+	 * @param p Priority of the added String.
+	 */
+	public void add(String s, int p) {
+		
+	}
+	
+	/**
+	 * Returns a String whose priority is maximum.
+	 * @return String with max priority.
+	 */
+	public String returnMax() {
+		return null;
+	}
+	
+	/**
+	 * Returns a String whose priority is maximum and removes it from the priority queue.
+	 * @return Maximum, extracted String.
+	 */
+	public String extractMax() {
+		return null;
+	}
+	
+	/**
+	 * Removes the element from the priority queue whose array index is i.
+	 * @param i Index of element in array to be removed.
+	 */
+	public void remove(int i) {
+		
+	}
+	
+	/**
+	 * Decrements the priority of the ith element by k.
+	 * @param i Element whose priority is to be decremented.
+	 * @param k Amount of decrease for the ith element.
+	 */
+	public void decrementPriority(int i, int k) {
+		
+	}
+	
+	/**
+	 * Returns an array B with the following property: B[i] = key(A[i]) for all i in the array A used to implement the priority queue.
+	 * @return Priority Array of the priority queue.
+	 */
+	public int[] priorityArray() {
+		return null;
+	}
+	
 }
