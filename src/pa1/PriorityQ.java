14db6b81db9eab79a0591539a2a82f22d420e63a
Alex <ans66@iastate.edu>
2018-09-30 22:20:03 -0500

    Methods and inner class
    Implemented the add and returnMax methods. Created inner class to
    encapsulate the string with its priority. Also created a swap method
    because I thought I needed it, but I don't yet lol xD

--- src/pa1/PriorityQ.java
+++ src/pa1/PriorityQ.java
@@ -7,11 +7,15 @@ package pa1;
  */
 public class PriorityQ {
 	
+	private Entry[] heapArray;
+	private int heapSize;
+	
 	/**
 	 * Creates an empty priority queue.
 	 */
 	public PriorityQ() {
-		
+		heapArray = new Entry[201]; // pretty sure the max # of strings is 200
+		heapSize = 0; // new priQ has no size
 	}
 	
 	/**
@@ -20,7 +24,16 @@ public class PriorityQ {
 	 * @param p Priority of the added String.
 	 */
 	public void add(String s, int p) {
+		Entry e = new Entry(s, p); // encapsulate the string and priority into an Entry
 		
+		int position = ++heapSize; //start position at the end of the heap
+		heapArray[position] = e; // put the new entry at the end of the heap
+		
+		while(position != 1 && p > heapArray[position/2].getPriority()) { // while the position isn't 1 and the priority of the new entry is greater than its parent
+			heapArray[position] = heapArray[position/2]; // move the parent down the heap
+			position /= 2; // the position moves to the parent
+		}
+		heapArray[position] = e; // assign the new entry at the index of the final position
 	}
 	
 	/**
@@ -28,7 +41,7 @@ public class PriorityQ {
 	 * @return String with max priority.
 	 */
 	public String returnMax() {
-		return null;
+		return heapArray[1].getAddress(); // highest priority is at the top of the heap
 	}
 	
 	/**
@@ -64,4 +77,37 @@ public class PriorityQ {
 		return null;
 	}
 	
+	/**
+	 * Helper method used for swapping entries
+	 * @param e1 First Entry to swap
+	 * @param e2 Second Entry to swap
+	 */
+	private static void swap(Entry e1, Entry e2) {
+		Entry temp = e1;
+		e1 = e2;
+		e2 = temp;
+	}
+	
+	/**
+	 * Private Class to encapsulate the address string and int priority into an Entry
+	 * @author Alexander Stephens
+	 *
+	 */
+	private class Entry{
+		private String address;
+		private int priority;
+		
+		protected Entry(String address, int priority) {
+			this.address = address;
+			this.priority = priority;
+		}
+		
+		private int getPriority() {
+			return priority;
+		}
+		
+		private String getAddress() {
+			return address;
+		}
+	}
 }
