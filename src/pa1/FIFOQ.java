148b8d6eed47c227b006247eb2e7ec7f457c5128
Alex <ans66@iastate.edu>
2018-10-02 10:30:42 -0500

    Finished queue

--- src/pa1/FIFOQ.java
+++ src/pa1/FIFOQ.java
@@ -1,35 +1,50 @@
 package pa1;
 
-public class FIFOQ implements Queue{
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-	@Override
-	public int getsize() {
-		// TODO Auto-generated method stub
-		return 0;
+
+public class FIFOQ implements Queue{
+	
+	private LinkedList<String> queue;
+	
+	public FIFOQ() {
+		queue = new LinkedList<String>();
 	}
 
 	@Override
-	public void add() {
+	public int getSize() {
 		// TODO Auto-generated method stub
+		return queue.size();
+	}
+
+	@Override
+	public void add(String s) throws Exception {
+		// TODO Auto-generated method stub
+		queue.add(s);
 		
 	}
 
 	@Override
 	public String returnMax() {
 		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return queue.getFirst();
 	}
 
 	@Override
 	public String extractMax() {
 		// TODO Auto-generated method stub
-		return null;
+		
+		return queue.removeFirst();
 	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return queue.size() == 0;
+	}
+	
+
+
 
 }
